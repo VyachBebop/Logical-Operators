23e0280609348337a836fb4a9ba7f7d39f75de96
VyachBebop <vyacheslavkolikov@gmail.com>
2025-02-11 19:21:42 +0300

    First Commit

---
+++ src/TaskFive.java
@@ -0,0 +1,7 @@
+public class TaskFive {
+    //TODO Задача 5: Вывод чисел в обратном порядке
+    // Напишите программу, которая выводит числа от N до 1 в обратном порядке
+    public static void main(String[] args) {
+
+    }
+}
